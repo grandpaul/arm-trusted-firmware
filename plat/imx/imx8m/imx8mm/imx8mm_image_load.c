0d92424042766cc7d2de51851247168933d2629b
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2019-04-17 16:07:11 +0100

    plat: imx8: Add image load logic for MBL TBB FIP booting
    This is a copy/paste of the WaRP7 code sufficient to compile for i.MX8.
    Further changes will be required when extracting elements from a real FIP.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ plat/imx/imx8m/imx8mm/imx8mm_image_load.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <bl_common.h>
+#include <desc_image_load.h>
+#include <platform.h>
+#include <platform_def.h>
+
+void plat_flush_next_bl_params(void)
+{
+	flush_bl_params_desc();
+}
+
+bl_load_info_t *plat_get_bl_image_load_info(void)
+{
+	return get_bl_load_info_from_mem_params_desc();
+}
+
+bl_params_t *plat_get_next_bl_params(void)
+{
+	return get_next_bl_params_from_mem_params_desc();
+}
